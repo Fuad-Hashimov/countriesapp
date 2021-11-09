730dba20a2b47b8b1a84cf8bf0126f44b8c1ebe0
FuadHashimov91 <fuadbh@code.edu.az>
2021-11-10 01:52:53 +0400

    Add Header Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
-import { AiOulineClose } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 import classes from "./Header.module.scss";
 
 const Header = () => {
+  const [menuOpen, setmenuOpen] = useState(true);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setmenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setmenuOpen(!menuOpen);
+  };
+
   return (
     <header className={classes.header}>
       <div className={classes.header_content}>
         <h2 className={classes.header_content_logo}>navbar</h2>
-        <nav className={classes.header_content_nav}>
+        <nav
+          className={`${classes.header_content_nav} ${
+            menuOpen ? classes.isMenu : ""
+          }`}
+        >
           <ul>
             <li>
               <a href="/">Page One</a>
@@ -19,11 +50,16 @@ const Header = () => {
             <li>
               <a href="/">Page Three</a>
             </li>
+            
           </ul>
           <button>CTA Page</button>
         </nav>
         <div className={classes.header_content_toggle}>
-          <BiMenuAltRight />
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
         </div>
       </div>
     </header>
